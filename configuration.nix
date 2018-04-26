1a04c8b59dc16fe83f46f75f3ac4d43c210b947d
Anton Plotnikov <plotnikovanton@gmail.com>
2018-04-26 19:38:06 +0300

    WG

--- configuration.nix
+++ configuration.nix
@@ -24,6 +24,7 @@
     hostName = "nixos";
     networkmanager.enable = true;
     firewall.allowedTCPPorts = [ 22 8888 ];
+    firewall.enable = false;
   };
 
   i18n = {
--- wg.nix
+++ wg.nix
@@ -2,15 +2,28 @@
 
 {
   networking.wireguard.interfaces = {
-    wg0-kaliwe = {
-      ips = [ "10.0.0.2/24" ];
+    wg0-kaliwe = let 
+      host = "89.40.118.250";
+      ip = "${pkgs.iproute}/bin/ip";
+      rules = action: [
+        "${ip} rule ${action} to 10.101.0.0/16 lookup main pref 30" # OpenWay subnet
+        "${ip} rule ${action} to ${host} lookup main pref 30"
+        "${ip} rule ${action} to all lookup 80 pref 40"
+        "${ip} route ${action} default dev wg0-kaliwe table 80"
+      ];
+
+    in {
+      ips = [ "10.10.10.4/32" ];
       privateKeyFile = "/root/nixos/wg/kaliwe";
+      allowedIPsAsRoutes = false;
+      postSetup = rules "add"; 
+      postShutdown = rules "del";
 
       peers = [
         {
           publicKey = "tP01hSbQlw5sy8jKPnfF/LjWWuggHSS/7H6Cf4ZI3AE=";
-          allowedIPs = [ "10.0.0.0/24" ];
-          endpoint = "kaliwe.ru:51820";
+          allowedIPs = [ "0.0.0.0/0" ];
+          endpoint = "${host}:51820";
           persistentKeepalive = 25;
         }
       ];
